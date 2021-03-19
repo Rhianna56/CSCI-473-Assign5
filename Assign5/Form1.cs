70b46c885d7285980fb55e7fb03f179fb2a6b8e9
Rhianna56 <58008379+Rhianna56@users.noreply.github.com>
2021-03-19 13:32:07 -0500

    Update Form1.cs

--- Assign5/Form1.cs
+++ Assign5/Form1.cs
@@ -1,3 +1,23 @@
+/*
+  _______________________________________________________________
+ /                                                               \
+||  Course: CSCI-473    Assignment #: 5  Semester: Spring 2021   ||
+||                                                               ||
+||  NAME1:  Rhianna Eberle                 Z-ID: z1848017        ||
+||                                                               ||
+||  NAME2:  Dillion Chappell                 Z-ID: z1761203      ||
+||                                                               ||
+||  NAME3: Karen Astorga                    Z-ID: z176117        ||
+||                                                               ||
+||  Professor: Daniel Rogness                                    ||
+||                                                               ||
+||  Due: Thursday 4/1/2021 by 11:59PM                            ||
+||                                                               ||
+||  Description:                                                 ||
+||     This file is the form file.                               ||
+||                                                               ||
+ \_______________________________________________________________/
+*/
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
